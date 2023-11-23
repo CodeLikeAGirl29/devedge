0d085c3e604abfd5c68c8941f0fed3da638c0ae6
codelikeagirl29 <codelikeagirl91@gmail.com>
2023-11-23 14:09:36 -0600

    more data

--- pages/index_creative.js
+++ pages/index_creative.js
@@ -1,46 +1,54 @@
 // import ItemIsotope from "../src/components/ItemIsotope";
-import dynamic from "next/dynamic";
-import TypingAnimation from "../src/components/TypingAnimation";
-import Layout from "../src/layout/Layout";
+import dynamic from 'next/dynamic';
+import TypingAnimation from '../src/components/TypingAnimation';
+import Layout from '../src/layout/Layout';
 
-const ItemIsotope = dynamic(() => import("../src/components/ItemIsotope"), {
-	ssr: false,
+const ItemIsotope = dynamic(() => import('../src/components/ItemIsotope'), {
+    ssr: false,
 });
 const IndexCreative = () => {
-	return (
-		<Layout>
-			{/* Section Started */}
-			<div className="section started layout-creative" id="section-started">
-				{/* background */}
-				<div className="video-bg">
-					<div className="video-bg-mask" />
-					<div className="video-bg-texture" id="grained_container" />
-				</div>
-				{/* started content */}
-				<div className="centralize full-width">
-					<div className="vertical-center">
-						<div className="started-content">
-							<h1 className="h-title">
-								Hello, {`I’m`} <strong>Lindsey Howard</strong>,Front-end
-								Developer with a keen eye to detail based in Fort Walton Beach,
-								Florida.
-							</h1>
-							<div className="h-subtitles" style={{ opacity: "1" }}>
-								<TypingAnimation extraClassName={"h-subtitle"} />
-								<span className="typed-subtitle" />
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			{/* Works */}
-			<div className="section works" id="section-portfolio">
-				<div className="content">
-					<ItemIsotope />
-					<div className="clear" />
-				</div>
-			</div>
-		</Layout>
-	);
+    return (
+        <Layout>
+            {/* Section Started */}
+            <div
+                className="section started layout-creative"
+                id="section-started"
+            >
+                {/* background */}
+                <div className="video-bg">
+                    <div className="video-bg-mask" />
+                    <div className="video-bg-texture" id="grained_container" />
+                </div>
+                {/* started content */}
+                <div className="centralize full-width">
+                    <div className="vertical-center">
+                        <div className="started-content">
+                            <h1 className="h-title">
+                                Hello, {`I’m`} <strong>Lindsey Howard</strong>,
+                                a persistent Front-end Developer based in Fort
+                                Walton Beach, Florida.
+                            </h1>
+                            <div
+                                className="h-subtitles"
+                                style={{ opacity: '1' }}
+                            >
+                                <TypingAnimation
+                                    extraClassName={'h-subtitle'}
+                                />
+                                <span className="typed-subtitle" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* Works */}
+            <div className="section works" id="section-portfolio">
+                <div className="content">
+                    <ItemIsotope />
+                    <div className="clear" />
+                </div>
+            </div>
+        </Layout>
+    );
 };
 export default IndexCreative;
